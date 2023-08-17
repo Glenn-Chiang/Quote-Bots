e2ea11d84276efd038585a0d5b9b207fd1aa998d
Glenn Chiang <glennchiang24@gmail.com>
2023-08-17 14:55:04 +0800

    Implemented registration handler

--- nietzsche_bot/main.py
+++ nietzsche_bot/main.py
@@ -5,12 +5,13 @@ from telegram import Bot, Update
 from telegram.ext import Application, CommandHandler
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env')
 BASE_URL = os.getenv('BASE_URL')
+
 
 async def main():
     bot = Bot(token=os.getenv('BOT_TOKEN'))
-    
+
     users = []
     for user in users:
         await send_quote(bot=bot, author_url_name='Friedrich_Nietzsche', chat_id=user.chat_id)
@@ -26,8 +27,6 @@ async def send_quote(bot: Bot, author_url_name: str, chat_id: str):
 
     await bot.send_message(chat_id=chat_id, text=full_quote)
 
-
-    
 
 if __name__ == 'main':
     asyncio.run(main())
--- nietzsche_bot/registration.py
+++ nietzsche_bot/registration.py
@@ -1,9 +1,16 @@
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import Application, CallbackContext, CommandHandler
 import requests
 import os
+import json
 from dotenv import load_dotenv
-load_dotenv()
+
+load_dotenv('.env')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+load_dotenv('../.env')
+BASE_URL = os.environ.get('BASE_URL')
+ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID')
+
 
 class User():
     def __init__(self, name, chat_id) -> None:
@@ -16,11 +23,26 @@ def register_user(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
 
     user = User(name=username, chat_id=chat_id)
-    base_url = os.environ.get('BASE_URL')
-    requests.post("/authors/Friedrich_Nietzsche/subscribers", json=user)
+    user_data = json.dumps(user)
 
-    update.message.reply_text(text="Registration successful")
+    # Send post request to quotes API to register user as a subscriber
+    try:
+        requests.post(
+            f"{BASE_URL}/authors/Friedrich_Nietzsche/subscribers", json=user_data)
+        update.message.reply_text(text="Registration successful")
+        message_admin(
+            bot=context.bot, message_text=f"{username} has subscribed to Nietzsche Bot!")
+
+    except Exception as err:
+        print('Error registering user:', err)
+        message_admin(bot=context.bot,
+                      message_text=f"Error registering {username}")
+
+
+def message_admin(bot: Bot, message_text: str):
+    bot.send_message(chat_id=ADMIN_CHAT_ID, text=message_text)
+
 
 def main():
-    app = Application.builder().token(os.getenv('BOT_TOKEN')).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('start', register_user))
