12c9c449fc51ec3a772307d4fa457b87cfe5d904
Glenn Chiang <glennchiang24@gmail.com>
2023-08-20 16:02:04 +0800

    Added dostoevsky bot

---
+++ my_bots/dostoevsky_bot/cron_job/main.py
@@ -0,0 +1,14 @@
+from quote_bots.send_quote import send_quote
+import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    await send_quote(author_name='Fyodor_Dostoevsky', bot_token=os.getenv('BOT_TOKEN'))
+
+def lambda_handler(event, context):
+    asyncio.run(main())
+
+if __name__=='__main__':
+    asyncio.run(main())
---
+++ my_bots/dostoevsky_bot/main/main.py
@@ -0,0 +1,19 @@
+from quote_bots.builder import build as build_bot
+from telegram import Update
+import asyncio
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def lambda_handler(event, context):
+    asyncio.run(main(event))
+
+
+async def main(event):
+    app = build_bot(bot_token=os.getenv('BOT_TOKEN'),
+                    author_name='Fyodor_Dostoevsky')
+    await app.initialize()
+    update = Update.de_json(json.loads(event['body']), app.bot)
+    await app.process_update(update=update)
