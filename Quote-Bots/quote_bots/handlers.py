378bdd6cca3fcf55c2e542eb7f75af6d76556e77
Glenn Chiang <glennchiang24@gmail.com>
2023-08-20 16:47:03 +0800

    Implemented http error handling (again!!)

--- Quote-Bots/quote_bots/handlers.py
+++ Quote-Bots/quote_bots/handlers.py
@@ -1,23 +1,10 @@
+from services import get_random_quote, subscribe, unsubscribe, get_subscribers
 import os
+import requests
 from telegram import Update, Bot
 from telegram.ext import CallbackContext
 from dotenv import load_dotenv
 load_dotenv()
-from services import get_random_quote, subscribe, unsubscribe, get_subscribers
-
-
-async def subscribeHandler(update: Update, context: CallbackContext):
-    author_name = context.bot_data["author_name"]
-    username = update.effective_user.username
-    chat_id = str(update.effective_chat.id)
-
-    user = {"username": username, "chat_id": chat_id}
-
-    subscribe(user=user, author_name=author_name)
-
-    await update.message.reply_text(text="Subscribed! Type /help for a list of commands")
-    await message_admin(
-        bot=context.bot, message_text=f"{username} has subscribed to Nietzsche Bot!")
 
 
 async def quoteHandler(update: Update, context: CallbackContext):
@@ -26,12 +13,8 @@ async def quoteHandler(update: Update, context: CallbackContext):
         quote = get_random_quote(author_name=author_name)
         await update.message.reply_text(text=quote)
 
-    except Exception as err:
-        print('Error getting quote:', err)
-
-        await update.message.reply_text(text="Error getting quote")
-
-        await message_admin(bot=context.bot, message_text=f"Error getting quote")
+    except requests.exceptions.HTTPError as error:
+        await update.message.reply_text(error["error"])
 
 
 async def helpHandler(update: Update, context: CallbackContext):
@@ -44,17 +27,45 @@ async def helpHandler(update: Update, context: CallbackContext):
 
 async def subscribersHandler(update: Update, context: CallbackContext):
     author_name = context.bot_data["author_name"]
-    subscribers = get_subscribers(author_name=author_name)
-    formatted_subscribers = ('\n').join([subscriber['username'] for subscriber in subscribers])
+
+    try:
+        subscribers = get_subscribers(author_name=author_name)
+    except requests.exceptions.HTTPError as error:
+        await update.message.reply_text(error["error"])
+
+    formatted_subscribers = ('\n').join(
+        [subscriber['username'] for subscriber in subscribers])
+
     await update.message.reply_text(text=formatted_subscribers)
+
+
+async def subscribeHandler(update: Update, context: CallbackContext):
+    author_name = context.bot_data["author_name"]
+    username = update.effective_user.username
+    chat_id = str(update.effective_chat.id)
+
+    user = {"username": username, "chat_id": chat_id}
+
+    try:
+        subscribe(user=user, author_name=author_name)
+        await update.message.reply_text(text="Subscribed! Type /help for a list of commands")
+        await message_admin(
+            bot=context.bot, message_text=f"{username} has subscribed to Nietzsche Bot!")
+    except requests.exceptions.HTTPError as error:
+        await update.message.reply_text(error["error"])
+
 
 async def unsubscribeHandler(update: Update, context: CallbackContext):
     author_name = context.bot_data["author_name"]
     username = update.effective_user.username
     chat_id = str(update.effective_chat.id)
-    unsubscribe(author_name=author_name, chat_id=chat_id)
-    await update.message.reply_text(text="Unsubscribed. To subscribe again, enter /start")
-    await message_admin(bot=context.bot, message_text=f"{username} has unsubscribed")
+
+    try:
+        unsubscribe(author_name=author_name, chat_id=chat_id)
+        await update.message.reply_text(text="Unsubscribed. To subscribe again, enter /start")
+        await message_admin(bot=context.bot, message_text=f"{username} has unsubscribed")
+    except requests.exceptions.HTTPError as error:
+        await update.message.reply_text(error["error"])
 
 
 async def message_admin(bot: Bot, message_text: str):
--- Quote-Bots/quote_bots/services.py
+++ Quote-Bots/quote_bots/services.py
@@ -9,6 +9,7 @@ BASE_URL = os.getenv('BASE_URL')
 def get_random_quote(author_name: str):
     response = requests.get(
         f'{BASE_URL}/authors/{author_name}/randomquote')
+    response.raise_for_status()
     quote_content = response.json()['content']
     full_quote = f"{quote_content}"
     return full_quote
@@ -16,13 +17,14 @@ def get_random_quote(author_name: str):
 
 def subscribe(author_name: str, user: dict):
     response = requests.post(f'{BASE_URL}/authors/{author_name}/subscribers', json=user)
-    return response
+    response.raise_for_status()
 
 
 def get_subscribers(author_name: str) -> list:
     response = requests.get(f'{BASE_URL}/authors/{author_name}/subscribers')
+    response.raise_for_status()
     return response.json()
 
 def unsubscribe(author_name: str, chat_id: str):
     response = requests.delete(f'{BASE_URL}/authors/{author_name}/subscribers/{chat_id}')
-    return response
+    response .raise_for_status()
