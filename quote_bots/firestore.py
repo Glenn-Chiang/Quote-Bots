19616d059bdd8f907dd831fd77504e40930587ba
Glenn Chiang <glennchiang24@gmail.com>
2023-08-20 10:38:27 +0800

    Deleted firestore initialization and credentials Will no longer be using firebase as it is incompatible with aws lambda production environment :(

--- quote_bots/firestore.py
+++
@@ -1,7 +0,0 @@
-from firebase_admin import initialize_app, firestore, credentials
-from google.cloud.firestore import Client
-
-
-cred = credentials.Certificate(r'C:\Users\glenn\Programming Projects\Quote_Bots\quote_bots\credentials.json')
-app = initialize_app(credential=cred)
-db: Client = firestore.client()
