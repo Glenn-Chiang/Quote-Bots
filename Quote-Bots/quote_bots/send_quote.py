3b1a7c5c92b4f6cc4025a7f7e1857e8237bf1014
Glenn Chiang <glennchiang24@gmail.com>
2023-08-21 14:04:08 +0800

    Added send_quote_to_channel

--- Quote-Bots/quote_bots/send_quote.py
+++ Quote-Bots/quote_bots/send_quote.py
@@ -2,8 +2,7 @@ from telegram import Bot
 from quote_bots.services import get_random_quote, get_subscribers
 
 
-async def send_quote(author_name: str, bot_token: str):
-    bot = Bot(token=bot_token)
+async def send_quote_to_user(author_name: str, bot: Bot):
     subscribers = get_subscribers(author_name=author_name)
     quote = get_random_quote(author_name=author_name)
 
@@ -13,5 +12,7 @@ async def send_quote(author_name: str, bot_token: str):
     print("Quote sent to subscribers")
     
 
-
-
+CHANNEL_ID = '@philosopher_quotes'
+async def send_quote_to_channel(author_name: str, bot: Bot):
+    quote = get_random_quote(author_name=author_name)
+    await bot.send_message(chat_id=CHANNEL_ID, text=quote)
--- my_bots/nietzsche_bot/cron_job/main.py
+++ my_bots/nietzsche_bot/cron_job/main.py
@@ -1,11 +1,14 @@
-from quote_bots.send_quote import send_quote
+from quote_bots.send_quote import send_quote_to_user, send_quote_to_channel
+from telegram import Bot
 import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 async def main():
-    await send_quote(author_name='Friedrich_Nietzsche', bot_token=os.getenv('BOT_TOKEN'))
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    await send_quote_to_channel(author_name='Friedrich_Nietzsche', bot=bot)
+    await send_quote_to_user(author_name='Friedrich_Nietzsche', bot=bot)
 
 def lambda_handler(event, context):
     asyncio.run(main())
